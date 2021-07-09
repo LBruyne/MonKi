721f266323b8f96936dfe1511226c4d85e8a27f3
LBruyne <519102931@qq.com>
2021-07-09 13:10:06 +0800

    后端项目建立

---
+++ backend/src/main/java/com/hinsliu/pianchang/PianchangApplication.java
@@ -0,0 +1,13 @@
+package com.hinsliu.pianchang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PianchangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PianchangApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/hinsliu/pianchang/PianchangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hinsliu.pianchang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PianchangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
