64a93ba30972c3b390a5970161b7709739862b1f
LBruyne <519102931@qq.com>
2021-07-09 17:19:26 +0800

    后端初次搭建

---
+++ backend/src/main/java/com/hinsliu/monki/Application.java
@@ -0,0 +1,13 @@
+package com.hinsliu.monki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkiApplication.class, args);
+	}
+
+}
