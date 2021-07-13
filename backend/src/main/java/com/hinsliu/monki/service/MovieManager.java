eff269084aef12e1a67b61c0b5a8b13fc30ea7cd
LBruyne <519102931@qq.com>
2021-07-13 21:24:10 +0800

    后端完成电影搜索接口

--- backend/src/main/java/com/hinsliu/monki/service/MovieManager.java
+++ backend/src/main/java/com/hinsliu/monki/service/MovieManager.java
@@ -1,8 +1,12 @@
 package com.hinsliu.monki.service;
 
+import com.hinsliu.monki.common.UtilConstant;
 import com.hinsliu.monki.common.enums.ErrorCodeEnum;
 import com.hinsliu.monki.common.exception.BusinessException;
 import com.hinsliu.monki.common.utils.copy.CopyUtils;
+import com.hinsliu.monki.common.utils.time.DateTimeUtil;
+import com.hinsliu.monki.dal.ClickActionDao;
+import com.hinsliu.monki.domain.model.ClickActionDO;
 import com.hinsliu.monki.domain.model.MovieDO;
 import com.hinsliu.monki.domain.query.MovieInfoQuery;
 import com.hinsliu.monki.domain.view.MovieDTO;
@@ -15,6 +19,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.stream.Collectors;
@@ -29,19 +34,45 @@ import java.util.stream.Collectors;
 public class MovieManager extends BaseManager {
 
     @Resource
+    private ClickActionDao clickActionDao;
+
+    @Resource
     private MongoTemplate mongoTemplate;
 
     public MovieDTO get(MovieInfoQuery query) {
         String id = query.getId();
         MovieDO movieDO = getOneMovieDocument(id);
+
+        // 记录用户点击行为
+        if (getUserId() != null) {
+            saveClickAction(movieDO);
+            log.info("记录用户{}的点击行为{}", getUserId(), movieDO.getName());
+        }
         return CopyUtils.MovieDOToMovieDTO(movieDO);
+    }
+
+    private void saveClickAction(MovieDO movieDO) {
+        ClickActionDO clickActionDO = new ClickActionDO();
+        clickActionDO.setUid(getUserId());
+        clickActionDO.setMid(movieDO.getId());
+        clickActionDO.setName(movieDO.getName());
+        clickActionDO.setGenre(String.join(UtilConstant.DEFAULT_DELIMITER, movieDO.getGenre()));
+        clickActionDO.setTime(DateTimeUtil.getCurrentDateTime());
+
+        int affectCount = clickActionDao.insert(clickActionDO);
+        if (affectCount > 0) {
+            log.info("插入用户点击记录，点击文档{}: {}", movieDO.getId(), affectCount);
+        } else {
+            log.warn("插入用户点击记录失败，点击文档{}", movieDO.getId());
+            throw new BusinessException(ErrorCodeEnum.FAIL.getCode(), "插入用户点击记录失败");
+        }
     }
 
     public MovieDO getOneMovieDocument(String id) {
         Query query = Query.query(Criteria.where("_id").is(new ObjectId(id)));
 
         MovieDO movieDO = mongoTemplate.findOne(query, MovieDO.class);
-        if(movieDO == null) {
+        if (movieDO == null) {
             log.warn("查找索引{}对应的电影文档失败", id);
             throw new BusinessException(ErrorCodeEnum.FAIL.getCode(), "查找索引对应的电影文档失败");
         } else {
@@ -50,7 +81,7 @@ public class MovieManager extends BaseManager {
         return movieDO;
     }
 
-    public MovieMetaDTO getMovieMeta(MovieDO movieDO){
+    public MovieMetaDTO getMovieMeta(MovieDO movieDO) {
         MovieDTO movieDTO = CopyUtils.MovieDOToMovieDTO(movieDO);
         return CopyUtils.MovieDTOToMovieMetaDTO(movieDTO);
     }
--- backend/src/main/java/com/hinsliu/monki/service/SearchEngineManager.java
+++ backend/src/main/java/com/hinsliu/monki/service/SearchEngineManager.java
@@ -1,8 +1,13 @@
 package com.hinsliu.monki.service;
 
+import com.hinsliu.monki.common.enums.ErrorCodeEnum;
 import com.hinsliu.monki.common.enums.SearchTypeEnum;
+import com.hinsliu.monki.common.exception.BusinessException;
+import com.hinsliu.monki.common.utils.time.DateTimeUtil;
+import com.hinsliu.monki.dal.SearchActionDao;
 import com.hinsliu.monki.domain.common.Page;
 import com.hinsliu.monki.domain.common.PageParam;
+import com.hinsliu.monki.domain.model.SearchActionDO;
 import com.hinsliu.monki.domain.query.RecommendQuery;
 import com.hinsliu.monki.domain.query.SearchQuery;
 import com.hinsliu.monki.domain.view.MovieMetaDTO;
@@ -31,6 +36,9 @@ public class SearchEngineManager extends BaseManager {
     @Resource
     private MovieManager movieManager;
 
+    @Resource
+    private SearchActionDao searchActionDao;
+
     public Page<MovieMetaDTO> search(SearchQuery query) {
         // 分页验证
         PageParam.verify(query);
@@ -41,12 +49,34 @@ public class SearchEngineManager extends BaseManager {
         Integer pageSize = query.getPageSize();
         Integer offset = query.getOffset();
 
+        // 保存用户搜索记录
+        if (getUserId() != null) {
+            saveSearchAction(kw, type);
+            log.info("记录用户{}的搜索行为{}", getUserId(), kw);
+        }
+
         // 通过ElasticSearch进行搜索，
         // 得到排名靠前结果对应的索引集合
         SearchResult result = searchES(kw, type, offset, pageSize);
 
         // 根据每个索引，找到对应的文档记录并返回;
         return new Page<>(page, pageSize, result.getCount(), searchMongo(result.getResults()));
+    }
+
+    private void saveSearchAction(String kw, SearchTypeEnum type) {
+        SearchActionDO searchActionDO = new SearchActionDO();
+        searchActionDO.setUid(getUserId());
+        searchActionDO.setContent(kw);
+        searchActionDO.setType(type.getVal());
+        searchActionDO.setTime(DateTimeUtil.getCurrentDateTime());
+
+        int affectCount = searchActionDao.insert(searchActionDO);
+        if (affectCount > 0) {
+            log.info("插入用户搜索记录，搜索关键字{}，类别{}: {}", kw, type.getName(), affectCount);
+        } else {
+            log.warn("插入用户搜索记录失败，搜索关键字{}，类别{}", kw, type.getName());
+            throw new BusinessException(ErrorCodeEnum.FAIL.getCode(), "插入用户搜索记录失败");
+        }
     }
 
     public Page<MovieMetaDTO> recommend(RecommendQuery query) {
