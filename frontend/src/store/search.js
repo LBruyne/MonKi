7f1d278ef025162c5416164f5fe8d6737b482955
Llux-C <244421824@qq.com>
2021-07-14 14:13:24 +0800

    添加电影id的全局state

--- frontend/src/store/search.js
+++ frontend/src/store/search.js
@@ -1,6 +1,7 @@
 export default{
     state:{
         search:window.localStorage.getItem('search')?window.localStorage.getItem('search'):undefined,
+        movieId:window.localStorage.getItem('movieId')?window.localStorage.getItem('movieId'):undefined,
     },
     getters:{
         search:(state)=>{
@@ -14,11 +15,26 @@ export default{
             }
             return state.search
         },
+        movieId:(state)=>{
+            if(state.movieId==undefined){
+                let sessionMovieId = localStorage.getItem("movieId");
+                if(sessionMovieId !=undefined)
+                {
+                    state.movieId = JSON.parse(sessionMovieId)
+                    return sessionMovieId
+                }
+            }
+            return state.movieId
+        },
     },
     mutations:{
         setSearch(state, search){
             localStorage.setItem("search",search)
             state.search=search
+        },
+        setMovieId(state, movieId){
+            localStorage.setItem("movieId",movieId)
+            state.movieId=movieId
         }
     },
     actions:{},
